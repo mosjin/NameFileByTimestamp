3086518aba2c75395dd3c0cc3ed97e80a42d9b13
mosjin <mosjin@gmail.com>
2019-11-26 10:53:46 +0800

    1. Added logic to handle files not created in the SAME day. 2. make files numbers one times can be handles to 100 million -1.

--- rename_files_by_timestamp.bash
+++ rename_files_by_timestamp.bash
@@ -3,14 +3,14 @@
 # rename files name by there creating timestamp on Windows/Linux.
 # mosjin
 
-oggs=$(ls -l --full-time  | awk -F" " '{print $7,$9}' | sort  | awk '{print $2}' | egrep -v "^[^a-zA-Z]" | egrep -v "^ *$" | grep -v "bash")
+oggs=$(ls -l --full-time  | awk -F" " '{print $6$7,$9}' | sort  | awk '{print $2}' | egrep -v "^[^a-zA-Z]" | egrep -v "^ *$" | grep -v "bash")
 num=1
 
 echo "Renaming..."
 for fil in ${oggs} 
 do
 
-	printf -v no "%03d" ${num}
+	printf -v no "%08d" ${num}
 	fileName=$(echo ${fil} | cut -d"_" -f1)
 	dateYear=$(echo ${fil} | cut -d"_" -f2)
 	dateTime=$(ls -l --full-time ${fil} | awk -F" " '{print $7}' | cut -d"." -f1 | sed 's/:[0-9]*$//' | sed 's/://')		
@@ -26,5 +26,5 @@ done
 
 
 echo ""
-ls -l --full-time | awk '{print $7, $9}' | sort | grep -v bash
+ls -l --full-time | awk '{print $6$7, $9}' | sort | grep -v bash
 echo Done!
